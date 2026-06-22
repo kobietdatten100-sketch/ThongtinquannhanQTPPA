c5ccac35f2d30f9fe1b1cafdee21ca95411b7d53
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-23 00:56:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+
+client.once('ready', ()=> console.log('Casino Bot Online'));
+client.on('messageCreate', msg => {
+ if(msg.author.bot) return;
+ if(!msg.content.startsWith('.')) return;
+ const cmd = msg.content.slice(1).split(/\s+/)[0].toLowerCase();
+ if(cmd==='help') msg.reply('Commands: .help .check .tx .dl .top');
+});
+client.login('YOUR_TOKEN');
