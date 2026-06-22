d11966dae915dd46b49d1bdc5dfb0e48a9395bc2
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-23 01:50:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,59 @@ function getUser(id) {
 
 client.once('ready', ()=> console.log('Casino Bot Online'));
 client.on('messageCreate', msg => {
+
+    if(msg.author.bot) return;
+    if(!msg.content.startsWith('.')) return;
+
+    const args = msg.content.slice(1).trim().split(/ +/);
+    const cmd = args.shift().toLowerCase();
+
+    if(cmd === "coins") {
+        const user = getUser(msg.author.id);
+        return msg.reply(`💰 Bạn có ${user.coins} Coins`);
+    }
+
+    
+    if(cmd === "profile") {
+if(cmd === "daily") {
+        if(cmd === "profile") {
+
+    const user = getUser(msg.author.id);
+
+    return msg.reply(
+        `👤 ${msg.author.username}\n💰 Coins: ${user.coins}`
+    );
+}
+        if(cmd === "pay") {
+
+    const target = msg.mentions.users.first();
+    const amount = parseInt(args[1]);
+
+    if(!target)
+        return msg.reply("Dùng: .pay @user 100");
+
+    const sender = getUser(msg.author.id);
+    const receiver = getUser(target.id);
+
+    if(sender.coins < amount)
+        return msg.reply("Không đủ tiền.");
+
+    sender.coins -= amount;
+    receiver.coins += amount;
+
+    saveData();
+
+    return msg.reply(
+        `💸 Đã chuyển ${amount} Coins cho ${target}`
+    );
+}
+
+});
+
  if(msg.author.bot) return;
  if(!msg.content.startsWith('.')) return;
- const cmd = msg.content.slice(1).split(/\s+/)[0].toLowerCase();
+ const args = msg.content.slice(1).trim().split(/ +/);
+const cmd = args.shift().toLowerCase();
  if(cmd==='help') msg.reply('Commands: .help .check .tx .dl .top');
 });
 client.login(process.env.TOKEN);
